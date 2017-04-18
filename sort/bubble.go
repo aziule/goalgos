3c85d00bdae5ee4104fa25c95e117bbf225503db
Will <willyboy_43@hotmail.com>
2017-04-18 14:21:03 +0100

    Replace the swap method for built-in feature

--- sort/bubble.go
+++ sort/bubble.go
@@ -6,7 +6,7 @@ func BubbleSort(elements []int) []int {
 
 	for i := 0; i < nbElements - 1; i++ {
 		if elements[i] > elements[i + 1] {
-			swap(elements, i, i + 1)
+			elements[i], elements[i + 1] = elements[i + 1], elements[i]
 			swapped = true
 		}
 	}
@@ -14,14 +14,6 @@ func BubbleSort(elements []int) []int {
 	if swapped {
 		return BubbleSort(elements)
 	}
-
-	return elements
-}
-
-func swap(elements []int, x, y int) []int {
-	tmp := elements[x]
-	elements[x] = elements[y]
-	elements[y] = tmp
 
 	return elements
 }
