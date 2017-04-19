dafd270e2941b8f7c420ddfca3a2d03844e448f5
Will <willyboy_43@hotmail.com>
2017-04-19 14:04:46 +0100

    Add a test for not found binary search

--- search/binary_test.go
+++ search/binary_test.go
@@ -3,16 +3,30 @@ package search
 import (
 	"testing"
 	"github.com/stretchr/testify/assert"
+	"errors"
 )
 
 func TestBinarySearch(t *testing.T) {
 	needle := 2
+	haystack := []int{1, 2, 3, 4, 5}
 	expectedIndex := 1
 
-	haystack := []int{1, 2, 3, 4, 5}
-	err, index := BinarySearch(needle, haystack)
+	index, err := BinarySearch(needle, haystack)
 
 	assert.Nil(t, err)
 	assert.Equal(t, expectedIndex, index)
 	assert.Equal(t, haystack[expectedIndex], needle)
+}
+
+func TestNotFoundBinarySearch(t *testing.T) {
+	needle := 999
+	haystack := []int{1, 2, 3, 4, 5}
+
+	index, err := BinarySearch(needle, haystack)
+
+	expectedError := errors.New("Needle \"999\" not found")
+
+	assert.Error(t, err)
+	assert.Equal(t, expectedError, err)
+	assert.Equal(t, 0, index)
 }
