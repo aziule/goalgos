a774550dd830b4c94da5df8f40dc9deb61c16eb4
Will <willyboy_43@hotmail.com>
2017-04-28 09:50:35 +0100

    Mutualise search algorithms tests

--- search/search_test.go
+++ search/search_test.go
@@ -6,27 +6,35 @@ import (
 	"errors"
 )
 
-func TestBinarySearch(t *testing.T) {
-	needle := 2
-	haystack := []int{1, 2, 3, 4, 5}
-	expectedIndex := 1
-
-	index, err := BinarySearch(needle, haystack)
-
-	assert.Nil(t, err)
-	assert.Equal(t, expectedIndex, index)
-	assert.Equal(t, haystack[expectedIndex], needle)
+// List of algorithms to be tested
+var searchAlgorithms = []func(needle int, haystack []int) (int , error) {
+	BinarySearch,
 }
 
-func TestNotFoundBinarySearch(t *testing.T) {
+var haystack = []int{1, 2, 3, 4, 5}
+
+func TestSearch(t *testing.T) {
+	needle := 2
+	expectedIndex := 1
+
+	for _, algo := range searchAlgorithms {
+		index, err := algo(needle, haystack)
+
+		assert.Nil(t, err)
+		assert.Equal(t, expectedIndex, index)
+		assert.Equal(t, haystack[expectedIndex], needle)
+	}
+}
+
+func TestSearchNotFound(t *testing.T) {
 	needle := 999
-	haystack := []int{1, 2, 3, 4, 5}
-
-	index, err := BinarySearch(needle, haystack)
-
 	expectedError := errors.New("Needle \"999\" not found")
 
-	assert.Error(t, err)
-	assert.Equal(t, expectedError, err)
-	assert.Equal(t, 0, index)
+	for _, algo := range searchAlgorithms {
+		index, err := algo(needle, haystack)
+
+		assert.Error(t, err)
+		assert.Equal(t, expectedError, err)
+		assert.Equal(t, 0, index)
+	}
 }
