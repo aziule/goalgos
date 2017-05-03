1e91e3b76c9391b4e3c8f8f75645e2ec831a6670
Will <willyboy_43@hotmail.com>
2017-05-03 13:18:55 +0100

    Add tests for arrays with duplicates

--- sort/sort_test.go
+++ sort/sort_test.go
@@ -13,11 +13,21 @@ var sortAlgorithms = []func(elements []int) []int {
 }
 
 // Test all of the algorithms
-func TestSort(t *testing.T) {
+func TestSortUnique(t *testing.T) {
 	expected := []int{1, 3, 4, 5, 7}
 
 	for _, algo := range sortAlgorithms {
 		elements := []int{5, 1, 4, 3, 7}
+		algo(elements)
+		assert.Equal(t, expected, elements, fmt.Sprintf("Expectations do not match the actual values."), expected, elements)
+	}
+}
+
+func TestSortWithDuplicates(t *testing.T) {
+	expected := []int{1, 1, 1, 3, 4, 4, 5, 7}
+
+	for _, algo := range sortAlgorithms {
+		elements := []int{1, 5, 1, 4, 3, 7, 1, 4}
 		algo(elements)
 		assert.Equal(t, expected, elements, fmt.Sprintf("Expectations do not match the actual values."), expected, elements)
 	}
